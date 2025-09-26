71417f1f5f1893765a1f011071bd0ef6c4490851
Carolina <carofacello.13@gmail.com>
2025-09-26 17:04:48 -0300

    Creamos AutomovilRepository y IAutomovil

---
+++ HybridDDDArchitecture/Infrastructure/Repositories/Sql/AutomovilRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Infraestructure.Repositories.Sql;
+
+namespace Infrastructure.Repositories.Sql
+{
+    internal class AutomovilRepository : BaseRepository<Automovil>, IAutomovilRepository
+    {
+        public AutomovilRepository(StoreDbContext context) : base(context) { }
+    }
+}
---
+++ HybridDDDArchitecture/Infrastructure/Repositories/Sql/IAutomovilRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Application.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Sql
+{
+    public interface IAutomovilRepository : IRepository<Automovil> { }
+}
