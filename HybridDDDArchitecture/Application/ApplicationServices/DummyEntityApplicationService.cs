822ec213d4aa8483d5909e0daf13fdad8837a7a4
Carolina <carofacello.13@gmail.com>
2025-09-29 01:01:44 -0300

    Corrección Service

--- HybridDDDArchitecture/Application/ApplicationServices/DummyEntityApplicationService.cs
+++ HybridDDDArchitecture/Application/ApplicationServices/DummyEntityApplicationService.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Repositories;
 
 namespace Application.ApplicationServices
 {
-
-    public interface IAutomovilApplicationService
+    /// <summary>
+    /// Ejemplo de un servicio de aplicacion para resolver procesos
+    /// relacionados a la entidad Dummy que no son responsabilidad del
+    /// handler que ejecuta el caso de uso.
+    /// </summary>
+    public class DummyEntityApplicationService(IDummyEntityRepository context) : IDummyEntityApplicationService
     {
-        bool AutomovilExists(string numeroChasis);
+        private readonly IDummyEntityRepository _context = context ?? throw new ArgumentNullException(nameof(context));
+
+        public bool DummyEntityExist(object value)
+        {
+            value = value ?? throw new ArgumentNullException(nameof(value));
+
+            var response = _context.FindOne(value);
+
+            return response != null;
+        }
     }
-
-
 }
-
